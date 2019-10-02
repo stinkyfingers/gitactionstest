c69aa807a9f4533446c1908a9dca4a073b89439b
John Shenk <john.shenk@rackspace.com>
2019-10-02 15:21:55 -0500

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,13 @@ resource "aws_s3_bucket" "bucket" {
     Environment = "dev"
   }
 }
+
+# backend
+terraform {
+  backend "s3" {
+    bucket = "remotebackend"
+    key    = "gitactionstest/terraform.tfstate"
+    region = "us-west-1"
+    profile = "jds"
+  }
+}
